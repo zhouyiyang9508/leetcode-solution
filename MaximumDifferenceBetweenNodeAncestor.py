dc2b476a78a243f1d4d27b73ff87745e5e6a4bb3
yiyangzhou <zhouyiyang9508@gmail.com>
2022-12-09 02:04:06 -0800

    added treenode implementaion, solved 1026

---
+++ MaximumDifferenceBetweenNodeAncestor.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+import TreeNode
+
+def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+    def helper(node, cur_max, cur_min):
+        if not node:
+            return cur_max - cur_min
+        cur_max = max(cur_max, node.val)
+        cur_min = min(cur_min, node.val)
+        left = helper(node.left, cur_max, cur_min)
+        right = helper(node.right, cur_max, cur_min)
+
+        return max(left, right)
+
+    return helper(root, root.val, root.val)
+
