c4b040b7be7f9b5d43e4697bbd01db1d97e288da
zhouyiyang9508 <zhouyiyang9508@gmail.com>
2022-12-16 21:27:52 -0500

    348. Design Tic-Tac-Toe

---
+++ DesignTic-Tac-Toe.py
@@ -0,0 +1,30 @@
+class TicTacToe:
+
+    def __init__(self, n: int):
+        self.n = n
+        self.rows = [0] * n
+        self.cols = [0] * n
+        self.diagonal = 0
+        self.anti_diagonal = 0
+
+    def move(self, row: int, col: int, player: int) -> int:
+        step = 1 if player == 1 else -1
+
+        self.rows[row] += step
+        self.cols[col] += step
+
+        # in the diagonal
+        if row == col:
+            self.diagonal += step
+        if row + col == self.n - 1:
+            self.anti_diagonal += step
+
+        # check if there is winner
+        if abs(self.rows[row]) == self.n or abs(self.cols[col]) == self.n or abs(self.diagonal) == self.diagonal or abs(self.anti_diagonal) == self.anti_diagonal:
+            return player
+
+        return 0
+
+    
+
+
