83213810a3d1d8ba25d1dbaa39f663c0c7cafc57
zhouyiyang9508 <zhouyiyang9508@gmail.com>
2022-12-26 12:58:29 -0500

    100. Same Tree

---
+++ SameTree.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from TreeNode import TreeNode
+
+
+def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if not p and not q:
+        return True
+    if not p or not q:
+        return False
+    if p.val != q.val:
+        return False
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
