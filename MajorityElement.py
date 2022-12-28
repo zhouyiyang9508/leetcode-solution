b5e7b14f54eaf32c96d10856109dac43cf62a834
zhouyiyang9508 <zhouyiyang9508@gmail.com>
2022-12-27 22:47:25 -0500

    169. Majority Element

---
+++ MajorityElement.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def majorityElement(nums: List[int]) -> int:
+    count = 0
+    candidate = 0
+    for i in range(len(nums)):
+        if count == 0:
+            candidate = nums[0]
+        count += (1 if candidate == nums[i] else - 1)
+    return candidate
+
+if __name__ == '__main__':
+    print(majorityElement([2,2,1,1,1,2,2]) == 2)
+
